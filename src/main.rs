21d90737388f1b0ce545ce52fcd6447fae87ca6d
J/A <archer884@gmail.com>
2018-11-08 14:37:58 -0600

    Add recurse param

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
-use std::io::Write;
-use std::{env, io};
+use std::io::{self, Write};
+use std::path::{Path, PathBuf};
+use structopt::StructOpt;
 use walkdir::WalkDir;
 
-fn main() -> io::Result<()> {
-    let path = env::args().nth(1);
-    let path = path.as_ref().map(AsRef::as_ref).unwrap_or(".");
+#[derive(StructOpt)]
+struct Opt {
+    #[structopt(parse(from_os_str))]
+    path: Option<PathBuf>,
+    #[structopt(short = "r", long = "recurse")]
+    recurse: bool,
+}
 
+impl Opt {
+    fn path(&self) -> &Path {
+        self.path.as_ref().map(AsRef::as_ref).unwrap_or_else(|| Path::new("."))
+    }
+}
+
+fn main() -> io::Result<()> {
+    let opt = Opt::from_args();
     let stream = io::stdout();
     let mut stream = stream.lock();
-
-    let walker = WalkDir::new(path).min_depth(1).max_depth(1);
+    
+    let walker = WalkDir::new(&opt.path()).min_depth(1);
+    let walker = if opt.recurse {
+        walker
+    } else {
+        walker.max_depth(1)
+    };
 
     for entry in walker.into_iter().filter_map(Result::ok) {
         writeln!(stream, "{}", entry.path().display())?;
